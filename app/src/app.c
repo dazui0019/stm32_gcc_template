a449dce911e8c296239dbd1c5a6866f0d9010e0f
dazui0019 <dazui0019@gmail.com>
2026-03-29 19:32:09 +0800

    feat: integrate printf_tiny output

--- app/src/app.c
+++ app/src/app.c
@@ -1,5 +1,9 @@
 #include "app.h"
 #include "main.h"
+#include "usart.h"
+#include "printf_tiny.h"
+
+uint8_t* hell_str = (uint8_t*)"Hello, World!\r\n";
 
 int setup(void)
 {
@@ -9,5 +13,11 @@ int setup(void)
 void loop(void)
 {
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-    HAL_Delay(100);
+    printf_("hello, world!\r\n");
+    HAL_Delay(500);
+}
+
+void _putchar(char character){
+    uint8_t ch = character;
+    HAL_UART_Transmit(&huart2, &ch, 1, 100);
 }
